04004b50481ec6ccfe1bccd3c56d74d4a2625084
jolugbo <jolugbofemi@gmail.com>
2018-05-11 22:38:57 +0100

    final commit

---
+++ README.md
@@ -0,0 +1,17 @@
+HackerBay.io backend interview
+=========
+
+Run the application : simply use nodemon to run the application
+==================
+
+Description
+======
+
+the short application performs an autentication, by colleting the username and password parmeter in the body, and generating a webtoken for subsequent request to other methods in the application
+
+the webtoken was implemented using the jsonwebtoken module
+
+jsonpatch used to create json api response
+
+jimp module was used for the image resize
+
--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const app = express();
 const jspatch = require('jsonpatch');
-
+const formidable = require('formidable');
+const fs = require('fs');
+const jimp = require('jimp');
 //configure app to encode call values
 app.use(bodyParser.urlencoded({ extended: false }))
 
+//default routes to login page
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/login.html');
+    fs.readFile('views/login.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+        return res.end();
+    });
 });
 
+//autentication handler
 app.post('/authenticate', (req, res) => {
     user = {};
     patch = [
@@ -20,30 +29,65 @@ app.post('/authenticate', (req, res) => {
         { "op": "add", "path": "/password", "value": req.body.password }
     ];
     userPatch = jspatch.apply_patch(user, patch);
-    //res.json(userPatch);
     jwt.sign({ userPatch }, 'secret', (err, token) => {
         res.json({ token });
     });
 });
 
-app.post('/upload', verification, (req, res) => {
+//upload form generator for test 
+app.get('/uploadForm', (req, res) => {
+    fs.readFile('views/uploadform.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+        return res.end();
+    });
+});
+
+//code to resize and return download link url
+app.post('/resizeAction', verification, (req, res) => {
     jwt.verify(req.token, 'secret', (err, authData) => {
         if (err) {
             res, sendStatus(403);
         } else {
-            res.json({
-                message: 'Post Created',
-                authData
+            var form = new formidable.IncomingForm();
+            var oldpath, newpath;
+            form.parse(req, function (err, fields, files) {
+                oldpath = files.filetoupload.path;
+                newpath = "uploadPath/"+ files.filetoupload.name;
+                fs.rename(oldpath, newpath, function (err) {
+                    if (err) throw err;
+                });
+                jimp.read(newpath).then(function (imgFile) {
+                    imgFile.resize(50, 50)
+                        .write(newpath); // save
+                }).catch(function (err) {
+                    console.error(err);
+                });
+                response = {};
+                patch = [
+                    { "op": "add", "path": "/message", "value": "Resize Completed Successfully" },
+                    { "op": "add", "path": "/imgResizeUrl", "value": newpath },
+                ];
+                responsePatch = jspatch.apply_patch(response, patch);
+                res.json({
+                    responsePatch,
+                    authData
+                });
+                res.end();
             });
         }
     })
 });
+
+//code to verify token validity
 function verification(req, res, next) {
     const bearerHeader = req.headers['authorization'];
     if (typeof bearerHeader !== 'undefined') {
         const bearer = bearerHeader.split(' ');
         const bearerToken = bearer[1];
         req.token = bearerToken;
+
         next();
     }
     else {
@@ -51,16 +95,3 @@ function verification(req, res, next) {
     }
 }
 app.listen(8080, function () { console.log('Server running...') });
-
-function authenticate(user){
-    user = {};
-    patch = [
-        { "op": "add", "path": "/username", "value": user.username },
-        { "op": "add", "path": "/password", "value": user.password }
-    ];
-    userPatch = jspatch.apply_patch(user, patch);
-    //res.json(userPatch);
-    jwt.sign({ userPatch }, 'secret', (err, token) => {
-        res.json({ token });
-    });
-}
